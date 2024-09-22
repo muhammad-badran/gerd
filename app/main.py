dccac9984471e52695d73e1afb52fb2ba968e9db
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2024-09-22 13:38:04 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,68 @@ import sys
 import os
 import time
 
+config = {
+    'app': {
+        'config': {
+            'name': 'full-stack-app'
+        }
+    },
+    'llm': {
+        'provider': 'openai',
+        'config': {
+            'model': 'gpt-4o-mini',
+            'temperature': 0.5,
+            'max_tokens': 1000,
+            'top_p': 1,
+            'stream': False,
+            'prompt': (
+                "Use the following pieces of context to answer the query at the end.\n"
+                "If you don't know the answer, just say that you don't know, don't try to make up an answer.\n"
+                "$context\n\nQuery: $query\n\nHelpful Answer:"
+            ),
+            'system_prompt': (
+                "Act as William Shakespeare. Answer the following questions in the style of William Shakespeare."
+            ),
+            'api_key': 'sk-xxx',
+            "model_kwargs": {"response_format": {"type": "json_object"}},
+            "http_client_proxies": "http://testproxy.mem0.net:8000",
+        }
+    },
+    'vectordb': {
+        'provider': 'chroma',
+        'config': {
+            'collection_name': 'full-stack-app',
+            'dir': 'db',
+            'allow_reset': True
+        }
+    },
+    'embedder': {
+        'provider': 'openai',
+        'config': {
+            'model': 'text-embedding-3-small',
+            'api_key': 'sk-xxx',
+            "http_client_proxies": "http://testproxy.mem0.net:8000",
+        }
+    },
+    'chunker': {
+        'chunk_size': 2000,
+        'chunk_overlap': 100,
+        'length_function': 'len',
+        'min_chunk_size': 0
+    },
+    'cache': {
+      'similarity_evaluation': {
+          'strategy': 'distance',
+          'max_distance': 1.0,
+      },
+      'config': {
+          'similarity_threshold': 0.8,
+          'auto_flush': 50,
+      },
+    },
+}
+
+
 sys.path.append(os.path.abspath('.'))
 
 import streamlit as st
@@ -16,7 +78,6 @@ def ingest_data_dynamic(n):
         gerd.add(url_)
 
     st.session_state["IS_CHATBOT_READY"] = True
-
 
 def response_embedchain(query):
     """Logic for loading the chain you want to use should go here."""
@@ -44,7 +105,7 @@ def add_form_row(row):
 
 
 def provide_data_dynamic():
-
+    
     with st.expander("Sources Data Form", expanded=st.session_state["expander_state"]):
         num_data_sources = st.slider('Number of Data Sources', min_value=1, max_value=10)
         for r in range(num_data_sources):
