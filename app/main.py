b398b4dc6c680ab78ab81c86dd6d94cf114994ea
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2024-09-22 15:22:47 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     if st.session_state.get("OPENAI_API_CONFIGURED") and st.session_state.get("submit_data_form"):
         st.markdown("Main App: Started")
         from embedchain import App
-        gerd = App()
+        gerd = App.from_config(configpath='config.py')
         # ingesting data
         if not st.session_state.get("IS_CHATBOT_READY"):
             with st.spinner('Ingesting Data! Please Wait!'):
