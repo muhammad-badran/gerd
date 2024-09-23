19582a6eb8e15a3b56db0cc31e5a090bfc1b23f8
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2024-09-23 02:06:34 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,127 +1,193 @@
-"""Python file to serve as the frontend"""
-import sys
-import os
-import time
-from embedchain import App
+# """Python file to serve as the frontend"""
+# import sys
+# import os
+# import time
+# from embedchain import App
 
-sys.path.append(os.path.abspath('.'))
+# sys.path.append(os.path.abspath('.'))
 
-import streamlit as st
-from app.components.sidebar import sidebar
+# import streamlit as st
+# from app.components.sidebar import sidebar
 
-def ingest_data_dynamic(n):
-    print(f'Number of Data Sources are {n}')
-    for r in range(n):
-        url_= st.session_state.get(f"value_{r}")
-        print(f"Ingestion {r+1}/{n}: {url_}")
-        gerd.add(url_)
+# def ingest_data_dynamic(n):
+#     print(f'Number of Data Sources are {n}')
+#     for r in range(n):
+#         url_= st.session_state.get(f"value_{r}")
+#         print(f"Ingestion {r+1}/{n}: {url_}")
+#         gerd.add(url_)
 
-    st.session_state["IS_CHATBOT_READY"] = True
+#     st.session_state["IS_CHATBOT_READY"] = True
 
-def response_embedchain(query):
-    """Logic for loading the chain you want to use should go here."""
-    print(f'Calling response on: {query}')
-    response = gerd.query(query, query_config)
-    return response
+# def response_embedchain(query):
+#     """Logic for loading the chain you want to use should go here."""
+#     print(f'Calling response on: {query}')
+#     response = gerd.query(query, query_config)
+#     return response
 
-def add_data_form(r):
-    st.session_state[f"url_{r}"] = [st.session_state.get(f"value_{r}")]
-    print(st.session_state.get(f"{r}"))
-
-
-def add_form_row(row):
-    # Inputs listed within a form
-    # loaders_type = ["youtube_video", "pdf_file", "web_page", "qna_pair", "text"]
-    data_form = st.form(key=f'{row}-Form')
-    with data_form:
-        data_columns = st.columns(1)
-        with data_columns[0]:
-            st.text_input(f"Enter Doc URL: {row}",
-                            value="https://www.youtube.com/watch?v=Xg1bqjKv-zg",
-                            key=f"value_{row}")
-        st.form_submit_button(on_click=add_data_form(row))
+# def add_data_form(r):
+#     st.session_state[f"url_{r}"] = [st.session_state.get(f"value_{r}")]
+#     print(st.session_state.get(f"{r}"))
 
 
-def provide_data_dynamic():
+# def add_form_row(row):
+#     # Inputs listed within a form
+#     # loaders_type = ["youtube_video", "pdf_file", "web_page", "qna_pair", "text"]
+#     data_form = st.form(key=f'{row}-Form')
+#     with data_form:
+#         data_columns = st.columns(1)
+#         with data_columns[0]:
+#             st.text_input(f"Enter Doc URL: {row}",
+#                             value="https://www.youtube.com/watch?v=Xg1bqjKv-zg",
+#                             key=f"value_{row}")
+#         st.form_submit_button(on_click=add_data_form(row))
+
+
+# def provide_data_dynamic():
     
-    with st.expander("Sources Data Form", expanded=st.session_state["expander_state"]):
-        num_data_sources = st.slider('Number of Data Sources', min_value=1, max_value=10)
-        for r in range(num_data_sources):
-            add_form_row(r)
-        submit_data_form = st.button("Submit Data", on_click=toggle_closed)
-        if submit_data_form:
-            st.session_state["submit_data_form"] = True
-        return num_data_sources
+#     with st.expander("Sources Data Form", expanded=st.session_state["expander_state"]):
+#         num_data_sources = st.slider('Number of Data Sources', min_value=1, max_value=10)
+#         for r in range(num_data_sources):
+#             add_form_row(r)
+#         submit_data_form = st.button("Submit Data", on_click=toggle_closed)
+#         if submit_data_form:
+#             st.session_state["submit_data_form"] = True
+#         return num_data_sources
 
 
-def toggle_closed():
-    st.session_state["expander_state"] = False
+# def toggle_closed():
+#     st.session_state["expander_state"] = False
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    st.set_page_config(
-        page_title="GRED Assistant Chatbot",
-        page_icon="📖",
-        layout="wide",
-        initial_sidebar_state="expanded", )
-    st.header("GRED Assistant Chatbot ")
+#     st.set_page_config(
+#         page_title="GRED Assistant Chatbot",
+#         page_icon="📖",
+#         layout="wide",
+#         initial_sidebar_state="expanded", )
+#     st.header("GRED Assistant Chatbot ")
 
-    sidebar()
+#     sidebar()
 
-    if "expander_state" not in st.session_state:
-        st.session_state["expander_state"] = True
+#     if "expander_state" not in st.session_state:
+#         st.session_state["expander_state"] = True
 
-    num_data_sources = provide_data_dynamic()
+#     num_data_sources = provide_data_dynamic()
 
-    if not st.session_state.get("OPENAI_API_CONFIGURED"):
-        st.error("Please configure your API Keys!")
+#     if not st.session_state.get("OPENAI_API_CONFIGURED"):
+#         st.error("Please configure your API Keys!")
 
-    if not st.session_state.get("submit_data_form"):
-        st.error("Please Submit the Data Form")
+#     if not st.session_state.get("submit_data_form"):
+#         st.error("Please Submit the Data Form")
 
-    if st.session_state.get("OPENAI_API_CONFIGURED") and st.session_state.get("submit_data_form"):
-        st.markdown("Main App: Started")
-        gerd = App()
-        # ingesting data
-        if not st.session_state.get("IS_CHATBOT_READY"):
-            with st.spinner('Ingesting Data! Please Wait!'):
-                # ingest_data(data_dict)
-                ingest_data_dynamic(num_data_sources)
-            st.success('Data ingestion has finished successfully!')
+#     if st.session_state.get("OPENAI_API_CONFIGURED") and st.session_state.get("submit_data_form"):
+#         st.markdown("Main App: Started")
+#         gerd = App()
+#         # ingesting data
+#         if not st.session_state.get("IS_CHATBOT_READY"):
+#             with st.spinner('Ingesting Data! Please Wait!'):
+#                 # ingest_data(data_dict)
+#                 ingest_data_dynamic(num_data_sources)
+#             st.success('Data ingestion has finished successfully!')
 
-        if st.session_state.get("IS_CHATBOT_READY"):
+#         if st.session_state.get("IS_CHATBOT_READY"):
 
-            if "messages" not in st.session_state:
-                st.session_state["messages"] = [
-                    {"role": "assistant", "content": "How can I help you?"}]
+#             if "messages" not in st.session_state:
+#                 st.session_state["messages"] = [
+#                     {"role": "assistant", "content": "How can I help you?"}]
 
-            # Display chat messages from history on app rerun
-            for message in st.session_state.messages:
-                with st.chat_message(message["role"]):
-                    st.markdown(message["content"])
+#             # Display chat messages from history on app rerun
+#             for message in st.session_state.messages:
+#                 with st.chat_message(message["role"]):
+#                     st.markdown(message["content"])
 
-            if user_input := st.chat_input("What is your question?"):
-                # Add user message to chat history
-                st.session_state.messages.append({"role": "user", "content": user_input})
-                # Display user message in chat message container
-                with st.chat_message("user"):
-                    st.markdown(user_input)
-                # Display assistant response in chat message container
-                with st.chat_message("assistant"):
-                    message_placeholder = st.empty()
-                    full_response = ""
-                with st.chat_message("assistant"):
-                    message_placeholder = st.empty()
-                    full_response = ""
+#             if user_input := st.chat_input("What is your question?"):
+#                 # Add user message to chat history
+#                 st.session_state.messages.append({"role": "user", "content": user_input})
+#                 # Display user message in chat message container
+#                 with st.chat_message("user"):
+#                     st.markdown(user_input)
+#                 # Display assistant response in chat message container
+#                 with st.chat_message("assistant"):
+#                     message_placeholder = st.empty()
+#                     full_response = ""
+#                 with st.chat_message("assistant"):
+#                     message_placeholder = st.empty()
+#                     full_response = ""
 
-                    with st.spinner('CHAT-BOT is at Work ...'):
-                        assistant_response = response_embedchain(user_input)
-                    # Simulate stream of response with milliseconds delay
-                    for chunk in assistant_response.split():
-                        full_response += chunk + " "
-                        time.sleep(0.05)
-                        # Add a blinking cursor to simulate typing
-                        message_placeholder.markdown(full_response + "▌")
-                    message_placeholder.markdown(full_response)
-                st.session_state.messages.append({"role": "assistant", "content": full_response})
+#                     with st.spinner('CHAT-BOT is at Work ...'):
+#                         assistant_response = response_embedchain(user_input)
+#                     # Simulate stream of response with milliseconds delay
+#                     for chunk in assistant_response.split():
+#                         full_response += chunk + " "
+#                         time.sleep(0.05)
+#                         # Add a blinking cursor to simulate typing
+#                         message_placeholder.markdown(full_response + "▌")
+#                     message_placeholder.markdown(full_response)
+#                 st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
+import os
+from embedchain import App
+import streamlit as st
+
+with st.sidebar:
+    huggingface_access_token = st.text_input("Hugging face Token", key="chatbot_api_key", type="password")
+    "[Get Hugging Face Access Token](https://huggingface.co/settings/tokens)"
+    "[View the source code](https://github.com/embedchain/examples/mistral-streamlit)"
+
+
+st.title("💬 Chatbot")
+st.caption("🚀 An Embedchain app powered by Mistral!")
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": """
+        Hi! I'm a chatbot. I can answer questions and learn new things!\n
+        Ask me anything and if you want me to learn something do `/add <source>`.\n
+        I can learn mostly everything. :)
+        """,
+        }
+    ]
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Ask me anything!"):
+    if not st.session_state.chatbot_api_key:
+        st.error("Please enter your Hugging Face Access Token")
+        st.stop()
+
+    os.environ["HUGGINGFACE_ACCESS_TOKEN"] = st.session_state.chatbot_api_key
+    app = App.from_config(config_path="config.yaml")
+
+    if prompt.startswith("/add"):
+        with st.chat_message("user"):
+            st.markdown(prompt)
+            st.session_state.messages.append({"role": "user", "content": prompt})
+        prompt = prompt.replace("/add", "").strip()
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            message_placeholder.markdown("Adding to knowledge base...")
+            app.add(prompt)
+            message_placeholder.markdown(f"Added {prompt} to knowledge base!")
+            st.session_state.messages.append({"role": "assistant", "content": f"Added {prompt} to knowledge base!"})
+            st.stop()
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("assistant"):
+        msg_placeholder = st.empty()
+        msg_placeholder.markdown("Thinking...")
+        full_response = ""
+
+        for response in app.chat(prompt):
+            msg_placeholder.empty()
+            full_response += response
+
+        msg_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
